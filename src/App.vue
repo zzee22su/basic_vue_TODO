657087455e705a4cc6a02a5da799223c49789254
zzee22su <patrickstar0910@gmail.com>
2022-06-07 11:16:45 +0900

    IMP : Todo 리스트 적기

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,37 @@
 <template>
-  <div id="app">
-    
+  <div id="app" class="container">
+    <h1 class="text-center">Todo App</h1>
+    <input v-model="todoText" type="text" placeholder="Type todo" @keyup.enter="addTodo">
+    <hr>
+    <Todo v-for="todo in todos" :key="todo.id" :todo="todo"></Todo>
   </div>
 </template>
 
 <script>
-
+import Todo from '@/components/Todo.vue'
 export default {
- 
+  components: {
+    Todo
+  },
+  data() {
+    return {
+      todoText: '',
+      todos : [
+        { id: 1, text: 'buy a car', checked: false},
+        { id: 2, text: 'play game', checked: false},
+      ]
+    }
+  },
+  methods: {
+    addTodo(e) {
+      this.todos.push({
+        id: Math.random(),
+        text: e.target.value,
+        checked: false
+      });
+      this.todoText = '';
+    }
+  }
 }
 </script>
 
---
+++ src/components/Todo.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+      <input type="checkbox" :checked="todo.checked">
+      <span>{{ todo.text }}</span>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        todo: {
+            type: Object,
+            required: true
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+
+</style>
