c34bcf3476bec1d88861437e00aae12358635f8c
zzee22su <patrickstar0910@gmail.com>
2022-06-20 18:41:26 +0900

    ADD : vuex 설치

---
+++ src/store/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+exported default new Vuex.Store({
+    state: {//데이터가 들어가는 곳 
+    
+    },
+    mutations: {//state에 있는 데이터를 바꾸는 것은 mutations에서만 가능하다. 
+
+    },
+    actions: {//함수가 들어가는 곳  -> 비동기적인 일을 처리한다. 
+
+    },
+    getters: {//vue 컴포넌트 안의 computed와 비슷한 속성. 
+
+    }
+
+})
