6877d026e2e02c16b536f627002225ac4a4620ec
zzee22su <patrickstar0910@gmail.com>
2022-06-20 12:47:35 +0900

    SIMPLIFY : App.vue 내 input 컴포넌트화

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,30 @@
+<template>
+  <diV>
+    <input 
+        v-model="todoText" 
+        type="text" 
+        placeholder="Type todo" 
+        @keyup.enter="addTodo">
+  </diV>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            todoText: '',
+        }
+    },
+    
+    methods: {
+        addTodo(e) {
+            this.$emit('add-todo', e.target.value);
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
