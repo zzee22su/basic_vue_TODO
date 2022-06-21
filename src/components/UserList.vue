620256669887b597980445685ab8d9bee8c26ed2
zzee22su <patrickstar0910@gmail.com>
2022-06-21 17:57:41 +0900

    REFACTOR : 함수화 처리

--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -18,11 +18,17 @@ export default {
     },
 
     created() {
-        axios.get('https://jsonplaceholder.typicode.com/users').then(response => {
+        this.getUsers();
+    },
+
+    methods: {
+        getUsers() {
+            axios.get('https://jsonplaceholder.typicode.com/users').then(response => {
             // 해당 주소로 요청을 보내고 응답이 온 후에 then을 처리한다. -> 비동기 작업
             // ex) axios.get 아래로 console.log()가 있다면 요청을 보내놓고 console.log를 찍고 응답이 오면 then을 처리한다.  
             this.users = response.data;
         });
+        }
     }
 }
 </script>
