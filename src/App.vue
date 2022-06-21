a8cb38672504328ecc744562e706a9852a19603d
zzee22su <patrickstar0910@gmail.com>
2022-06-21 17:41:25 +0900

    ADD : UserList 추가

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
     <AddTodo></AddTodo>
     <hr>
     <TodoList></TodoList>
+    <UserList></UserList>
   </div>
 </template>
 
@@ -12,45 +13,13 @@
 import TodoList from '@/components/TodoList.vue'
 import AddTodo from '@/components/AddTodo.vue'
 import CompltedTodo from '@/components/CompletedTodo.vue'
+import UserList from '@/components/UserList.vue'
 
 export default {
   components: {
-    TodoList, AddTodo, CompltedTodo
+    TodoList, AddTodo, CompltedTodo, UserList
   },
-  data() {
-    return {
-      todoText: '',
-      // todos : [
-      //   { id: 1, text: 'buy a car', checked: false},
-      //   { id: 2, text: 'play game', checked: false},
-      // ]
-    }
-  },
-  methods: {
-    // addTodo(value) {
-    //   this.todos.push({
-    //     id: Math.random(),
-    //     text: value,
-    //     checked: false
-    //   });
-    //   this.todoText = '';
-    // },
-    // toggleCheckbox({id, checked}) {
-    //   const index = this.todos.findIndex(todo => {
-    //     return todo.id === id;
-    //   });
-    //   this.todos[index].checked = checked;
-    // },
-    // deleteTodo(id) {
-      // 방법1
-      // const index = this.todos.findIndex(todo => {
-      //   return todo.id === id;
-      // });
-      // this.todos.splice(index, 1);
-      // 방법2
-      // this.todos = this.todos.filter(todo => todo.id !== id);
-    // }
-  }
+
 }
 </script>
 
